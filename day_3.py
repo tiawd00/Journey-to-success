722d408e059d3939b1ab06deefaef52cead69ed5
Tiawd <227413977+tiawd00@users.noreply.github.com>
2025-08-21 22:30:41 +0300

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -1,13 +1,14 @@
 import time
-print("you are a baby in comunist Artstostka or sm shi. \n" 
-"the day you are born is 1979 or sm shi.\n" 
-"   You have exactly 99¢ to your name.\n" 
-"You can either buy bread to survive or do nothing, cuz you a baby!\n")
+print("\")
 
 x = str(input("what are you going to do?\n" \
-"You'r options are: 'Buy bred' or 'say nothing' "))
+"Your options are: 'Buy bred' or 'say nothing' "))
 
 
 statements = ["Buy bred"]
-if input==str(statements): print("yo shit survived.")
+if input==str(statements): print("\")
+
 else: print("Oh, no, the baby died from a curb stomp.")
+
+
+# idk how to make ts work🥀
