883b6ef8475e4405399ac119bbeaf7a22af2143f
Tiawd <227413977+tiawd00@users.noreply.github.com>
2025-08-22 22:12:57 +0300

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -1,14 +1,1 @@
-import time
-print("\")
 
-x = str(input("what are you going to do?\n" \
-"Your options are: 'Buy bred' or 'say nothing' "))
-
-
-statements = ["Buy bred"]
-if input==str(statements): print("\")
-
-else: print("Oh, no, the baby died from a curb stomp.")
-
-
-# idk how to make ts work🥀
