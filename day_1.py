d9ef50ca43a0e6fdfe318b73b2abf165bff08210
Tiawd <227413977+tiawd00@users.noreply.github.com>
2025-08-22 22:12:25 +0300

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -1,18 +1,1 @@
 
-introduction = "first out of many!"
-centering = introduction.center(80)
-print(centering)
-
-
-
-
-cores = 8
-
-class pc:
-    gpu = "Gt1030"
-    cpu = "i5-3550 4 cores"
-    ram = f"{cores}GB ram"
-
-Main_pc = pc()
-
-print(f"  You have like, uh, {pc.ram} running with a {pc.gpu} and finally a {pc.cpu}.\n Yep, that's all.💔")
